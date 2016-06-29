942878a02eca0afe61ddebd38739479a808c79fd
flyingkite <982727566@qq.com>
2016-06-29 13:30:36 +0800

    complete

--- order/printer/try_print.py
+++ order/printer/try_print.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from escpos import *
 import datetime
+import serial
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -8,17 +9,25 @@ sys.setdefaultencoding('utf8')
 def print_usb(dish_list):
     usb = printer.Usb(0x0485, 0x7541, 0, out_ep=0x03)
     usb.set(align='center')
-    now = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+    now = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S\n")
     usb.text(now)
-    usb.text("order_id:3\n")
     usb.text("desk_id:3\n")
+    usb.set(align='left')
     total=0
     for dish in dish_list:
         dish_num=int(dish["num"])
         dish_price=float(dish["price"])
-        usb.text("%s num:%d price:%f\n"%(dish["name"],dish_num,dish_price))
+        usb.text("\t%s\tnum:%d\tprice:%.2f$\n"%(dish["name"],dish_num,dish_price))
         total+=dish_num*dish_price
-    usb.text("total:%f\n"%total)
+    usb.set(align='right')
+    usb.text("total:%.2f$\n"%total)
     usb.cut()
     usb.close()
 
+
+def print_stm():
+    ser=serial.Serial("/dev/ttyUSB0",9600,timeout=0.4)
+    ser.write("h")
+
+if __name__ == '__main__':
+    print_stm()
--- order/views.py
+++ order/views.py
@@ -10,21 +10,23 @@ def index(request):
 def submit(requset):
     dish_list=list()
     dishs={
-        "value1":["",""],
-        "value2":["",""],
-        "value3":["",""],
-        "value4":["",""],
-        "value5":["",""],
+        "value1":["Cake","20"],
+        "value2":["Salmon","17"],
+        "value3":["Bread","8"],
+        "value4":["Fish","13"],
+        "value5":["Bacon","22"],
     }
     for dish in dishs:
-        if dish in requset.GET:
+        if dish in requset.GET and int(requset.GET[dish]):
             adish=dict()
             adish["name"]=dishs[dish][0]
             adish["num"]=requset.GET[dish]
             adish["price"]=dishs[dish][1]
             dish_list.append(adish)
-    print(dish_list)
+    printer.print_usb(dish_list)
+    printer.print_stm()
     return HttpResponseRedirect("redirect")
 
+
 def redirect(requset):
-    pass
+    return render(requset,"Myredirect.html")
